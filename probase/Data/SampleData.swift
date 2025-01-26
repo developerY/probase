a1a93fce92830a17286cda9d12f2503afa1bba65
Developer YLabz <developer@ylabz.com>
2025-01-25 17:20:43 -0800

    detail view are working

--- probase/Data/SampleData.swift
+++ probase/Data/SampleData.swift
@@ -12,14 +12,11 @@ import SwiftData
 class SampleData {
     static let shared = SampleData()
 
-
     let modelContainer: ModelContainer
-
 
     var context: ModelContext {
         modelContainer.mainContext
     }
-
 
     private init() {
         let schema = Schema([
@@ -28,26 +25,19 @@ class SampleData {
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: true)
 
-
         do {
             modelContainer = try ModelContainer(for: schema, configurations: [modelConfiguration])
-
-
             insertSampleData()
-
-
             try context.save()
         } catch {
             fatalError("Could not create ModelContainer: \(error)")
         }
     }
 
-
     private func insertSampleData() {
             for friend in Friend.sampleData {
                 context.insert(friend)
             }
-
 
             for movie in Movie.sampleData {
                 context.insert(movie)
